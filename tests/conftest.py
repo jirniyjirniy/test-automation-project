47af18bd9c70397b8907ab2b3a0c1ad78cd3ffc9
jirniyjirniy <nik.vardi127@gmail.com>
2025-03-25 19:48:08 +0200

    Com8

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,6 @@ from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
 def browser():
-    import playwright
-    playwright.install()
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         yield browser
